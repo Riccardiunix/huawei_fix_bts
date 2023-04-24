66bd66f597989c703a09717032f862f702f614e6
riccardiunix <riccardo.bordon.2014@gmail.com>
2023-04-24 14:09:09 +0200

    better handling of broken run

--- main.py
+++ main.py
@@ -6,41 +6,45 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.firefox.options import Options
 import time
 IP='192.168.1.1'
-PSW = '123456'
-ENB_ID = 1111111
-with Connection('http://admin:{}@{}/'.format(PSW,IP)) as connection:
+PSW='123456'
+ENB_ID=111111
+with Connection(f'http://admin:{PSW}@{IP}/') as connection:
     client = Client(connection)
-    if (int(hex(int(client.device.signal()["cell_id"]))[0:-2], 16) != ENB_ID):
+    enbid = int(hex(int(client.device.signal()["cell_id"]))[0:-2], 16)
+    ant_type = int(client.device.antenna_set_type()['antennasettype'])
+    if enbid != ENB_ID or ant_type != 2:
         options = Options()
         options.add_argument('--headless')
         options.page_load_strategy = 'eager'
         driver = webdriver.Firefox(options=options)
-        driver.get('http://{}/html/index.html'.format(IP))
+        driver.get(f'http://{IP}/html/index.html')
         print("Login")
-        WebDriverWait(driver,10).until(EC.element_to_be_clickable( ("id","login_btn")))
+        WebDriverWait(driver,15).until(EC.element_to_be_clickable( ("id","login_btn")))
         driver.find_element("id","login_password").send_keys(PSW)
         driver.find_element("id","login_btn").click()
-        WebDriverWait(driver,10).until(EC.presence_of_element_located(("id","menu_top_home")))
-        driver.get('http://{}/html/content.html#systemsettings'.format(IP))
-        WebDriverWait(driver,10).until(EC.element_to_be_clickable(("id","antenna_control")))
-        print("Antenna 1")
+        WebDriverWait(driver,15).until(EC.presence_of_element_located(("id","menu_top_home")))
+        driver.get(f'http://{IP}/html/content.html#systemsettings')
+        WebDriverWait(driver,15).until(EC.element_to_be_clickable(("id","antenna_control")))
         driver.find_element("id","antenna_control").click()
-        driver.find_element("id","antenna_mode_select").click()
-        driver.find_element("id","antenna_mode_select_list_item_1").click()
-        driver.find_element("id","antenna_save_button").click()
+        if enbid != ENB_ID:
+            print("Set Antenna EXT")
+            driver.find_element("id","antenna_mode_select").click()
+            driver.find_element("id","antenna_mode_select_list_item_1").click()
+            driver.find_element("id","antenna_save_button").click()
+            count = 0
+            while (int(hex(int(client.device.signal()["cell_id"]))[0:-2], 16) != ENB_ID or count > 15):
+                time.sleep(1)
+                count += 1
+            WebDriverWait(driver,15).until(EC.visibility_of_element_located(("id","antenna_mode_select")))
+        print("Set Antenna INT")
         count = 0
-        while (int(hex(int(client.device.signal()["cell_id"]))[0:-2], 16) != ENB_ID or count > 10):
-            time.sleep(1)
-            count += 1
-        WebDriverWait(driver,10).until(EC.visibility_of_element_located(("id","antenna_mode_select")))
-        print("Antenna 2")
-        flag = True
-        while flag:
+        while count < 15:
             try:
                 driver.find_element("id","antenna_mode_select").click()
                 driver.find_element("id","antenna_mode_select_list_item_2").click()
                 driver.find_element("id","antenna_save_button").click()
-                flag = False
+                count = 15
             except Exception as e:
                 time.sleep(1)
+                count += 1
         driver.quit()
